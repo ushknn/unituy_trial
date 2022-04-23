59d31ca7dd9b91ff905e7099e6013b00c1b4c261
ushknn <uekiworks@gmail.com>
2022-04-23 20:47:38 +0900

    Debug.Log() from test.txt

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using System.IO;
 
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, world");        
+        TextAsset textasset = new TextAsset(); //テキストファイルのデータを取得するインスタンスを作成
+        textasset = Resources.Load("Test", typeof(TextAsset) )as TextAsset; //Resourcesフォルダから対象テキストを取得
+        Debug.Log(textasset.text);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
